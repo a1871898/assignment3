20c211afb0046fe089bbf5a35e802ee12ecc0a00
peter <a1871898@adelaide.edu.au>
2023-10-30 23:07:26 +1030

    network

--- assignment3.py
+++ assignment3.py
@@ -56,27 +56,34 @@ shared_list.print_book("The Apple")
 shared_list.print_book("The Road to Oz")
 
 
-import unittest
-import threading
-import time
+import argparse
 
+# Assuming the Node and SharedList classes are defined here
 
-# Define your class and methods for the shared list
+# Add the following code to handle command-line arguments
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process command line arguments.')
+    parser.add_argument('-p', action='store_true', help='Allow the option to be passed to the script.')
+    args = parser.parse_args()
 
+    if args.p:
+        print("Option -p is allowed.")
 
-class TestSharedList(unittest.TestCase):
-    def setUp(self):
-        # Define any setup procedures here
-        pass
+    # Rest of your code here
+    shared_list = SharedList()
 
-    def test_thread_safety(self):
-        # Define your test for thread safety
-        pass
+    # Add nodes to the shared list
+    books = ["Robin Hood", "The Apple", "The Road to Oz"]
+    for book in books:
+        shared_list.add_node(book)
 
-    def test_multiple_streams(self):
-        # Define your test for handling multiple input streams
-        pass
+    # Simulating multiple reads
+    shared_list.head.book_next = Node("Page 1 of Robin Hood")
+    shared_list.head.book_next.book_next = Node("Page 2 of Robin Hood")
+    shared_list.head.next.book_next = Node("Page 1 of The Apple")
+    shared_list.head.next.next.book_next = Node("Page 1 of The Road to Oz")
 
-
-if __name__ == '__main__':
-    unittest.main()
+    # Print each book
+    shared_list.print_book("Robin Hood")
+    shared_list.print_book("The Apple")
+    shared_list.print_book("The Road to Oz")
