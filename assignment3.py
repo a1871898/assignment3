bb5f8fb50585f3747769f8f36095d35ccdd80872
peter <a1871898@adelaide.edu.au>
2023-10-30 23:11:16 +1030

    network2

--- assignment3.py
+++ assignment3.py
@@ -1,3 +1,45 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.book_next = None
+        self.next_frequent_search = None
+
+
+class SharedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.counter = 1
+
+    def add_node(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        print(f"Node with data '{data}' added to the shared list.")
+
+    def print_book(self, book_name):
+        current = self.head
+        file_name = f"book_{str(self.counter).zfill(2)}.txt"
+        with open(file_name, "w") as f:
+            while current:
+                if hasattr(current, 'book_next'):
+                    f.write(current.data + "\n")
+                    current = current.book_next
+                else:
+                    current = current.next
+            self.counter += 1
+        print(f"Book written to {file_name}")
+
+
+# Example usage
+shared_list = SharedList()
+
+
 
 
 import argparse
