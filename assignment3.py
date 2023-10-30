5518b1674b51feec68094a16cd634d74e71589eb
peter <a1871898@adelaide.edu.au>
2023-10-30 23:00:45 +1030

    task 2

--- assignment3.py
+++ assignment3.py
@@ -54,3 +54,58 @@ shared_list.head.next.next.book_next = Node("Page 1 of The Road to Oz")
 shared_list.print_book("Robin Hood")
 shared_list.print_book("The Apple")
 shared_list.print_book("The Road to Oz")
+
+
+import threading
+import time
+
+# Define the shared data structure and other necessary classes
+
+class AnalysisThread(threading.Thread):
+    def __init__(self, shared_list, search_pattern, interval):
+        super(AnalysisThread, self).__init__()
+        self.shared_list = shared_list
+        self.search_pattern = search_pattern
+        self.interval = interval
+        self.running = True
+
+    def run(self):
+        while self.running:
+            time.sleep(self.interval)
+            frequency_dict = {}
+            current = self.shared_list.head
+            while current:
+                if self.search_pattern in current.data:
+                    frequency_dict[current.data] = frequency_dict.get(current.data, 0) + 1
+                current = current.next
+
+            sorted_books = sorted(frequency_dict.items(), key=lambda x: x[1], reverse=True)
+
+            if sorted_books:
+                print(f"Most frequent occurrences of '{self.search_pattern}':")
+                for book, count in sorted_books:
+                    print(f"Book: {book}, Count: {count}")
+                print("\n")
+
+
+# Example usage
+shared_list = SharedList()  # Assuming you have already defined the SharedList class
+
+# Simulate the addition of nodes to the shared list
+books = ["Robin Hood", "The Apple", "The Road to Oz"]
+for book in books:
+    shared_list.add_node(book)
+
+# Create and start multiple analysis threads
+search_pattern = "to"  # Example search pattern, you can set this from the command line
+interval_seconds = 5  # Example interval, you can configure this according to your requirements
+
+analysis_thread_1 = AnalysisThread(shared_list, search_pattern, interval_seconds)
+analysis_thread_2 = AnalysisThread(shared_list, search_pattern, interval_seconds)
+
+analysis_thread_1.start()
+analysis_thread_2.start()
+
+# Allow the threads to run for a specified duration or until a stopping condition is met
+
+# Properly handle thread termination and clean up resources when the program ends
