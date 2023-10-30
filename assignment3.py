bb825b4f3d4d959445c850f39ea6893e42056726
peter <a1871898@adelaide.edu.au>
2023-10-30 23:20:44 +1030

    scale

--- assignment3.py
+++ assignment3.py
@@ -1,3 +1,17 @@
+import argparse
+
+# Assuming the Node and SharedList classes are defined here
+
+# Add the following code to handle command-line arguments
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process command line arguments.')
+    parser.add_argument('-l', dest='port', type=int, help='Specify the port number.')
+    args = parser.parse_args()
+
+    if args.port:
+        print(f"Using port: {args.port}")
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -39,37 +53,19 @@ class SharedList:
 # Example usage
 shared_list = SharedList()
 
+# Add nodes to the shared list
+books = ["Robin Hood", "The Apple", "The Road to Oz"]
+for book in books:
+    shared_list.add_node(book)
 
+# Simulating multiple reads
+shared_list.head.book_next = Node("Page 1 of Robin Hood")
+shared_list.head.book_next.book_next = Node("Page 2 of Robin Hood")
+shared_list.head.next.book_next = Node("Page 1 of The Apple")
+shared_list.head.next.next.book_next = Node("Page 1 of The Road to Oz")
 
+# Print each book
+shared_list.print_book("Robin Hood")
+shared_list.print_book("The Apple")
+shared_list.print_book("The Road to Oz")
 
-import argparse
-
-# Assuming the Node and SharedList classes are defined here
-
-# Add the following code to handle command-line arguments
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process command line arguments.')
-    parser.add_argument('-p', action='store_true', help='Allow the option to be passed to the script.')
-    args = parser.parse_args()
-
-    if args.p:
-        print("Option -p is allowed.")
-
-    # Rest of your code here
-    shared_list = SharedList()
-
-    # Add nodes to the shared list
-    books = ["Robin Hood", "The Apple", "The Road to Oz"]
-    for book in books:
-        shared_list.add_node(book)
-
-    # Simulating multiple reads
-    shared_list.head.book_next = Node("Page 1 of Robin Hood")
-    shared_list.head.book_next.book_next = Node("Page 2 of Robin Hood")
-    shared_list.head.next.book_next = Node("Page 1 of The Apple")
-    shared_list.head.next.next.book_next = Node("Page 1 of The Road to Oz")
-
-    # Print each book
-    shared_list.print_book("Robin Hood")
-    shared_list.print_book("The Apple")
-    shared_list.print_book("The Road to Oz")
